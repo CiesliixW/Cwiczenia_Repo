369b962d469308da57ef700ca90b349d08c8168b
Wojciech Cieslik <cieslikw.96@gmail.com>
2020-09-23 20:32:32 +0200

    Calculator done

--- Simple__calculator.py
+++ Simple__calculator.py
@@ -21,18 +21,28 @@ def ask_for_a_number(force_valid_input):
             return user_input
         except: 
             print("That didn't look like a number, try again!")
-    if force_valid_input == False: 
-        return None 
+    if force_valid_input == False:
+        try: 
+            user_input = float(input("Provide a number: "))
+            return user_input
+        except: 
+            return None 
         
 
 def ask_for_an_operator(force_valid_input):
     while force_valid_input == True:
-        user_operator = input("Provide an operator one of *,-,/,+")
+        user_operator = input("Provide an operator: ")
         if user_operator == "+" or "-" or "/" or "*":
-            return user_operator 
+            return user_operator
+        else: 
+            print("Invalid operator, try again!")
     
     if force_valid_input == False:
-        return None
+        user_operator = input("Provide an operator: ")
+        if user_operator == "+" or "-" or "/" or "*":
+            return user_operator
+        else: 
+            return None
 
 
 
@@ -40,17 +50,40 @@ def calc(operator, a, b):
     if not is_valid_operator(operator) or not is_number(a) or not is_number(b):
         return None
     
-    result None
+    result = None
     if operator == "+":
         result = a + b
         return result 
-    elif opertator == "-"
+    elif operator == "-":
         result == a - b 
-    elif 
+        return result
+    elif operator == "/":
+        if b == 0:
+            print("Error, nie można dzielic przez 0")
+            return None
+        else:
+            result = a / b
+            return result
+    elif operator == "*":
+        result = a * b
+        return result
+
+
+        
 
 def simple_calculator():
-    pass
+    while True: 
+        a = ask_for_a_number(force_valid_input=False)
+        if not a:
+            break 
+        
+        op = ask_for_an_operator(force_valid_input=True)
 
+        b = ask_for_a_number(force_valid_input = True)
+
+        result = calc(op, a, b)
+ 
+        print(result)
 
 if __name__ == '__main__':
     simple_calculator()
